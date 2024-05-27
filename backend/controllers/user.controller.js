d0db8825893f21dd1a15bf42bd65c08b3bca194f
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-05-27 18:27:56 +0200

    update user with db and server

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,9 +1,29 @@
-
 const User = require("../models/user.model");
-const UserModel = require("../models/user.model");
 
-module.exports.getUser= async (req, res)=> {
-  const user = new User(req.body)
-  await user.save()
-  res.status(200).json(user)
-}
+
+const getUser = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
+};
+
+const createUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(400).json({ message: 'Email déjà utilisé' });
+    } else {
+      res.status(500).json({ message: 'Erreur serveur' });
+    }
+  }
+};
+
+module.exports = { getUser, createUser };
