571133a3c318f24f08f7839863b26ac4512e8073
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-05-28 17:52:31 +0200

    coorection routes addUser

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { setUser } = require("../controllers/user.controller");
 const router = express.Router();
 
-router.post("/", setUser);
+router.post("/addUser", setUser);
 
 
 module.exports = router;
