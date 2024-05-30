ceb20c07b1907f4aace71bec9e1d425a54f91362
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-05-30 15:10:46 +0200

    authmiddleware on the road get

--- backend/routes/get.routes.js
+++ backend/routes/get.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { createUser, getUserById,} = require("../controllers/user.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 const router = express.Router();
 
-router.get("/user/:id", getUserById, createUser);
+router.get("/user/:id", authMiddleware, getUserById, createUser);
 
 
 module.exports = router;
